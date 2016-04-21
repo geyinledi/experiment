84e2189cd2f42724287a3a3c488cbb38c4b233c3
geyinledi <260221580@qq.com>
2016-04-21 11:55:22 +0800

    add HashTree

--- HashTree.cpp
+++ HashTree.cpp
@@ -1,19 +1,55 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+struct node
+{
+    int num,pwd;
+    node *next;
+    node(int x, node *p = NULL):num(x),next(p) {}
+};
 int main()
 {
-    int n,sum=0,i,k;
-    while(scanf("%d",&n)!=EOF&&n!=0)
+    int n,m,i,j;
+    char s[10];
+    printf("Please input the number of people:\n");
+    while(scanf("%d",&n)&&!n)printf("n can't is 0, please input new number.\n");
+    printf("Everyone's password is same?(Y/N)\n");
+    scanf("%s",s);
+    int flag=(strcmp(s,"Y")==0?1:0);
+    printf("Please input password:\n");
+    if(flag)scanf("%d",&m);
+    node *p, *q, *start;
+    i=1;
+    while(i<=n)
     {
-        int a[n];
-        for(i=0;i<n;i++)
-            scanf("%d",&a[i]);
-        for(i=0;i<n;i++)
-        {
-            k=a[i]/100+a[i]%100/50+(a[i]%100-a[i]%100/50*50)/10+a[i]%10/5+(a[i]%10-a[i]%10/5*5)/2+(a[i]%10-a[i]%10/5*5)%2;
-            sum=sum+k;
-        }
-        printf("%d\n",sum);
-        sum=0;
+        p = new node(i++);
+        if(flag)p->pwd = m;
+        else scanf("%d",&p->pwd);
+        if(i==2)start = p, m = start->pwd;
+        else q->next = p;
+        q = p;
     }
+    p = start;
+    q->next = p;
+
+    //solve
+    printf("Output order is:\n");
+    i=1,j=0;
+    while(j<n)
+    {
+        if(i == m)
+        {
+            if(j)printf(" -> ");
+            printf("%d",p->num);
+            m = p->pwd;
+            q->next = p->next;
+            delete(p);
+            p = q->next;
+            j++;
+            i = 1;
+        }
+        else
+            i++, p=p->next, q=q->next;
+    }
+    printf("\n");
+
     return 0;
 }
